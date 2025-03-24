cdcbe7932fa510fa38cad6b23f44d67dfdf7e3a5
Harsh Yadav <harshyadav235741@gmail.com>
2025-03-24 10:13:35 +0530

    first

---
+++ arithmeticoperators.js
@@ -0,0 +1,22 @@
+//+,-,*,/ %,i++,i--,N^n =,==,+=,-=,%=,+=,>,<,<=<>=<
+let a=5;
+let b=7;
+console.log(a+b);
+console.log(a/b);
+console.log(a%b)
+console.log(a-b)
+console.log(a*b)
+console.log(a^b)
+let c=1;
+c++;
+console.log(c)
+++c;
+console.log(c)
+let d=5;
+d++;
+console.log(d)
+let e=3;
+let f=5;
+f+=e;
+console.log(f)
+// logical operator And.OR,Not etc,
---
+++ datatypes.js
@@ -0,0 +1,11 @@
+//numbers string boolean undefined null bigint symbol
+
+const student={
+    name : "harsh yadav",
+    age:25,
+    cgpa:6.7,
+
+}
+console.log(student["name"]);
+console.log(student.age);
+console.log(student);
