cd74224e0825323430572cccdfb3af8b4642eaac
Harsh Yadav <harshyadav235741@gmail.com>
2025-03-24 10:08:51 +0530

    firstcommit

---
+++ varibles.js
@@ -0,0 +1,23 @@
+age=25;
+console.log(age);
+name="harsh yadav"
+console.log(name);
+price=15.40;
+console.log(price);
+x=null;
+console.log(x);
+//boolean
+isfollow=true;
+console.log(isfollow);
+//varibles name are case sensitive
+fullname="harsh";
+fullName="yadav"
+console.log(fullname);
+//differnce between N and n
+//let const and varible
+var age=25;
+var age=30;
+console.log(age);
+const age=45;
+//const age=44;error cannot be re-declared and update
+console.log(age);
